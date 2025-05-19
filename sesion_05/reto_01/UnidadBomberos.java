ecc6e6ccabbb88b018a45877512bec0553ab8961
K G B <kargb@ciencias.unam.mx>
2025-05-19 17:55:10 -0600

    estoy retrasada

--- sesion_05/reto_01/UnidadBomberos.java
+++ sesion_05/reto_01/UnidadBomberos.java
@@ -4,10 +4,30 @@ public class UnidadBomberos extends UnidadEmergencia{
     private SistemaGPS gps;
     private Sirena sirena;
     private Operador operador;
+
+    public UnidadBomberos(String nombre, String operador) {
+        super(nombre);
+        this.gps = new SistemaGPS();
+        this.sirena = new Sirena();
+        this.operador = new Operador(operador);
+    }
     
+    public void iniciarOperacion() {
+        activarUnidad();
+        gps.localizar();
+        sirena.activarSirena();
+        operador.reportarse();
+    }
+
     //Sobreescribir el método
     @Override
-    void activarUnidad() {
-        System.out.println(nombre + "Camión de bomberos en ruta para su destino.")
+    public void activarUnidad() {
+        System.out.println(nombre + "Camión de bomberos en ruta para su destino.");
+    }
+    @Override
+    public void responder() {
+        System.out.println(nombre + "Unidad de bomberos respondiendo a incendio estructural");
     }
 }
+ 
+    
