a00e085ddb347139b40eeb9fe78334cb2891c61f
K G B <kargb@ciencias.unam.mx>
2025-05-22 19:54:15 -0600

    Mi primer hilo

---
+++ M_2/sesion_01/sesion_02/Introduccion/HiloPrincipalMain.java
@@ -0,0 +1,10 @@
+package M_2.sesion_01.sesion_02.Introduccion;
+
+public class HiloPrincipalMain {
+    public static void main (String [] args) {
+        MiPrimerHilo primerHilo = new MiPrimerHilo();
+        // Nota: Ejecutar .start() y no .run()
+        primerHilo.start();
+    }
+    
+}
--- M_2/sesion_01/sesion_02/Introduccion/MiPrimerHilo.java
+++ M_2/sesion_01/sesion_02/Introduccion/MiPrimerHilo.java
@@ -1,6 +1,13 @@
 package M_2.sesion_01.sesion_02.Introduccion;
 
 public class MiPrimerHilo extends Thread{
-    
+    // el método run se ejecutará de manera concurrente
+
+    @Override
+        public void run() {
+            for (int i = 0; i < 1000; i++) {
+                System.out.println("=>" +i);
+            }
+        }
 
 }
